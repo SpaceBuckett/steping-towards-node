a66fa8cde200213ca279aa256d73899fd9fda737
SpaceBuckett <shehxadv2.0@gmail.com>
2023-03-28 14:34:06 +0500

    now displaying temperature and chances of rain on terminal

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,8 +2,10 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=507911120bed9261a377e7cbbadf95ea&query=34.0151,71.5249';
 
-request({ url: url }, (error, response) => {
-    // console.log(response);
-    const data = JSON.parse(response.body);
-    console.log(data.current);
-})  
+request({ url: url, json: true }, (error, response) => {
+
+    console.log("It is currently " + response.body.current.temperature + " degress out. There are " + response.body.current.precip + "% chances of rain.");
+})
+
+
+// It is currently 58 degress out. There are 0% changes of rain.
